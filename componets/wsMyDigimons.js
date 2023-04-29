99e16489a745f7617a8cc075926629fa1200b900
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-04-29 18:15:02 -0500

    feat: :sparkles: Section Galeria de Digimons
    Finalizacion de la seccion de la galeria funcional con la API, incluyendo workers y estilos

--- componets/wsMyDigimons.js
+++ componets/wsMyDigimons.js
@@ -1,7 +1,21 @@
 export let wsMyDigimons = {
-
+    displayDigimons(p1){
+        
+        return`
+            <div class="digimon">
+                        <div class="digimon-imagen">
+                        <img src="${p1.img}" alt="${p1.name}" width="50px">
+                        </div>
+                        <div class="digimon-info">
+                            <div class="nombre-contenedor">
+                                <h2 class="digimon-nombre">${p1.name}</h2>
+                                <p class="digimon-id">#${p1.level}</p>
+                            </div>
+                        </div>   
+                    <div>
+            `
+    }
 }
-
 
 self.addEventListener("message", (e)=>{
     postMessage(wsMyDigimons[`${e.data.module}`](e.data.data));
--- js/myDigimons.js
+++ js/myDigimons.js
@@ -1,26 +1,55 @@
 
+function muestraDigimons(digimons) {
+    const wsDigimons = new Promise((resolve) => {
+        const ws = new Worker("./componets/wsMyDigimons.js", {type: "module"});
+        let id = [];
+        let count = 0;
+        ws.postMessage({module:"displayDigimons", data: digimons}) 
+        id = [".digimons"]
+        ws.addEventListener("message", (e)=>{
+            let doc = new DOMParser().parseFromString(e.data, "text/html");
+            document.querySelector(id[count]).append(...doc.body.children);
+            (id.length-1==0) ? ws.terminate(): count++;
+        if (count == 2) {
+            resolve();
+          }  
+        });
+                  
+    });
+    wsDigimons.then(() => {
+ 
+    });
+}
 export default{
     showInterfazDigimons(){
         async function getGames(){
-            const url = 'https://digimon-api.vercel.app/api/digimon';
+            
+            const url = 'https://digimon-api.vercel.app/api/digimon?limit=30';
             try {
                 const response = await fetch(url);
-                const result = await response.json();
+                let result = await response.json();
+                result = result.slice(0, 51);
+                result.forEach(val => {
+                    muestraDigimons(val) 
+                });      
                 console.log(result);
             } catch (error) {
                 console.error(error);
             }
-            const apiKey = 'ff31eeb8709b4c9dbaf5a640c17003e3';
-            const url2 = `https://api.rawg.io/api/games?key=${apiKey}&search=digimon`;
-        
-            fetch(url2)
-            .then(response => response.json())
-            .then(data => {
-                // Aquí puedes trabajar con los datos que devolvió la API
-                console.log(data);
-            })
-            .catch(error => console.log(error));
-        
-            }
+        }
+
+        getGames();
     }
 }
+
+
+// const apiKey = 'ff31eeb8709b4c9dbaf5a640c17003e3';
+//             const url2 = `https://api.rawg.io/api/games?key=${apiKey}&search=digimon`;
+        
+//             fetch(url2)
+//             .then(response => response.json())
+//             .then(data => {
+//                 // Aquí puedes trabajar con los datos que devolvió la API
+//                 console.log(data);
+//             })
+//             .catch(error => console.log(error));
